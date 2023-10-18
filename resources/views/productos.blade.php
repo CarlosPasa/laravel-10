65b2cb03d5dae5029bd9d5efcc6284e112f8f63e
unknown <cap_13_96@hotmail.com>
2023-10-18 14:14:50 -0500

    Curso Laravel 10 capitulo 2.5

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,20 +1,4 @@
 @extends('_layouts.layout')
 @section('main')
-    <h1>PRODUCTOS</h1>
-    <div class="container">
-        <div class="row">
-            @foreach($productos as $producto)
-            <div class="col-12 col-sm-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $producto }}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
+    @include('_includes.productos')
 @endsection('main')
