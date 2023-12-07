fb0b28a4d4eb4c0f8479ba93f5c70697bbda258d
unknown <cap_13_96@hotmail.com>
2023-12-07 10:57:40 -0500

    Curso Laravel 10 capitulo 3.5

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+    //function que trae el modelo de categoria a productos
+    public function categoria(){
+        return $this->belongsTo(Categoria::class,'categoria_id');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,23 @@
         <div class="row">
             @foreach($categorias as $categoria)
             <div class="col-12 col-sm-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $categoria->nombre }}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                <h4>{{$categoria->nombre}}</h4>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    @foreach($categoria->productos as $producto)
+                    <div class="col-12 col-sm-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $producto->nombre }}</h5>
+                            </div>
+                        </div>
+            </div>
+                    @endforeach
                 </div>
             </div>
+            
             @endforeach
         </div>
     </div>
