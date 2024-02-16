5051ec01ff708de10e69e51ad2f5ce4aec0be26c
unknown <cap_13_96@hotmail.com>
2024-02-16 09:15:44 -0500

    Curso Laravel 10 capitulo 9.1

--- resources/js/components/ProcesoDeCompra/Productos.vue
+++ resources/js/components/ProcesoDeCompra/Productos.vue
@@ -1,31 +1,49 @@
 <template>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th>Subtotal</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="producto in store.productos" >
-                <td>{{ producto.nombre }}</td>
-                <td><input type="number" class="form-control" 
-                    @change="store.editarCantidad(producto.id, $event.target.value)"
-                    :value="producto.cantidad"></td>
-                <td>{{ producto.precio }}</td>                
-                <td>{{ producto.precio * producto.cantidad }}</td>
-            </tr>
-        </tbody>
-    </table>
-</template>
-<script setup>
-// Store
-import {useCarritoStore} from "@/stores/carrito";
+	<div class="row">
+		<div class="col-12 col-sm-9">
+			<table class="table">
+				<thead>
+				<tr>
+					<th>Nombre</th>
+					<th>Cantidad</th>
+					<th>Precio</th>
+					<th>Subtotal</th>
+				</tr>
+				</thead>
 
+				<tbody>
+				<tr
+					v-for="producto in store.productos">
+					<td>{{ producto.nombre }}</td>
+					<td>
+						<input type="number" class="form-control"
+							   @change="store.editarCantidad(producto.id, $event.target.value)"
+							   :value="producto.cantidad" />
+					</td>
+					<td>${{ producto.precio }}</td>
+					<td>${{ producto.precio * producto.cantidad }}</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div class="col-12 col-sm-3">
+			<Total />
+		</div>
+	</div>
+
+</template>
+
+<script setup>
+// Imports
+import {useCarritoStore} from "@/stores/carrito";
+import Total from "@/components/ProcesoDeCompra/Total.vue";
+
+// Store
 const store = useCarritoStore();
+
 </script>
+
 <style scoped>
-    
+
 </style>
---
+++ resources/js/components/ProcesoDeCompra/Total.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="card">
+            <h5 class="card-header">TOTAL</h5>
+            <div class="card-body">
+                <h6>{{  total   }}</h6>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+// Imports
+import { ref, watch } from 'vue';
+import { useCarritoStore } from '@/stores/carrito';
+import { storeToRefs } from 'pinia';
+
+// Store
+const store = useCarritoStore();
+
+// Data
+const total = ref(0);
+const { productos } = storeToRefs(store);
+
+watch(productos, () =>{
+    console.log("Se cambiaron los productos");
+    // Se hace llamado a la API para calcular el total
+
+},{ deep:true});
+
+</script>
