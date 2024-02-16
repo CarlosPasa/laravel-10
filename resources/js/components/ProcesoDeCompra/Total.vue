a2c4b7700f6c4ec50fb2ce31497364477421bcd2
unknown <cap_13_96@hotmail.com>
2024-02-16 11:07:58 -0500

    Curso Laravel 10 capitulo 9.2

--- resources/js/components/ProcesoDeCompra/Total.vue
+++ resources/js/components/ProcesoDeCompra/Total.vue
@@ -1,18 +1,17 @@
 <template>
-    <div>
-        <div class="card">
-            <h5 class="card-header">TOTAL</h5>
-            <div class="card-body">
-                <h6>{{  total   }}</h6>
-            </div>
-        </div>
-    </div>
+	<div class="card">
+		<h5 class="card-header">TOTAL</h5>
+		<div class="card-body">
+			<h6>${{ total }}</h6>
+		</div>
+	</div>
 </template>
+
 <script setup>
 // Imports
-import { ref, watch } from 'vue';
-import { useCarritoStore } from '@/stores/carrito';
-import { storeToRefs } from 'pinia';
+import {ref, watch} from "vue";
+import {useCarritoStore} from "@/stores/carrito";
+import {storeToRefs} from "pinia";
 
 // Store
 const store = useCarritoStore();
@@ -21,10 +20,27 @@ const store = useCarritoStore();
 const total = ref(0);
 const { productos } = storeToRefs(store);
 
-watch(productos, () =>{
-    console.log("Se cambiaron los productos");
-    // Se hace llamado a la API para calcular el total
+watch(productos, () => {
 
-},{ deep:true});
+	axios
+		.post('/api/carrito/calcular-total', {
+			productos: store.productos,
+		})
+		.then( (responseTotal) => {
+
+			total.value = responseTotal.data.total;
+
+		}).catch( (err) => {
+			console.error(err);
+		});
+
+
+}, {
+	deep: true,
+});
 
 </script>
+
+<style scoped>
+
+</style>
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 Route::get('productos/{producto}',[ProductoController::class,'show']);
+
+Route::prefix('carrito')->group(function() {
+    Route::post('calcular-total', [\App\Http\Controllers\API\CarritoController::class, 'calcularTotal']);
+    Route::post('finalizar-compra', [\App\Http\Controllers\API\CarritoController::class, 'finalizarCompra']);
+});
